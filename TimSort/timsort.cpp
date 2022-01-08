b1bc39eb9955d5602b7d337a149963dc43170198
String Max <mr.tens@foxmail.com>
2022-01-08 22:36:53 +0800

    Fix a bug in upperbound and add TODOs.

--- TimSort/timsort.cpp
+++ TimSort/timsort.cpp
@@ -2,103 +2,109 @@
 
 template <typename T>
 class TimSort {
-    using size_t = unsigned long long;
-    using Comparer = bool(*)(const T &, const T &);
+	using size_t = unsigned long long;
+	using Comparer = bool(*)(const T &, const T &);
 public:
-    TimSort();
-    ~TimSort();
-    static void sort(T [], size_t, Comparer=def_cmp);
+	TimSort();
+	~TimSort();
+	void sort(T [], size_t, Comparer=def_cmp);
 private:
-    T temp, *arr_helper;
-    size_t max_size;
-    static bool def_cmp(const T &, const T &);
-    static void swap(T &, T&);
-    static size_t minRunLength(size_t);
-    static size_t upperbound(const T [], const T &, size_t, size_t, Comparer);
-    static void insertSort(T [], size_t, Comparer);
+	T temp, *arr_helper;
+	size_t max_size;
+	static bool def_cmp(const T &, const T &);
+	void swap(T &, T&);
+	size_t minRunLength(size_t);
+	size_t upperbound(const T [], const T &, size_t, size_t, Comparer);
+	void insertSort(T [], size_t, Comparer);
 };
 
 template <typename T>
 TimSort<T>::TimSort() {
-    max_size = (1 << 9);
-    arr_helper = new T[max_size];
+	max_size = (1 << 9);
+	arr_helper = new T[max_size];
 }
 
 template <typename T>
 TimSort<T>::~TimSort() {
-    delete[] arr_helper;
-    max_size = 0;
+	delete[] arr_helper;
+	max_size = 0;
 }
 
 template <typename T>
 void TimSort<T>::sort(T arr[], size_t len, Comparer cmp) {
-    if(len <= 1) {
-        return;
-    }
+	if(len <= 1) {
+		return;
+	}
 
-    while(max_size <= len) {
-        if(max_size == 0) {
-            max_size = 1;
-        }
-        else {
-            max_size <<= 1;
-        }
-    }
+	while(max_size <= len) {
+		if(max_size == 0) {
+			max_size = 1;
+		}
+		else {
+			max_size <<= 1;
+		}
+	}
 
-    /* If len <= 64, just run insertSort. */
-    if(len <= (1 << 6)) {
-        insertSort(arr, len, cmp);
-        return;
-    }
+	/* If len <= 64, just run insertSort. */
+	if(len <= (1 << 6)) {
+		insertSort(arr, len, cmp);
+		return;
+	}
+
+	/* TODO: Slide the arr to different Run. */
+
+	/* TODO: GALLOP Mode. */
+
+	/* TODO: Less memory use method. */
 }
 
 template <typename T>
 bool TimSort<T>::def_cmp(const T &a, const T &b) {
-    return a < b;
+	return a < b;
 }
 
 template <typename T>
 void TimSort<T>::swap(T &a, T &b) {
-    temp = a, a = b, b = temp;
+	temp = a, a = b, b = temp;
 }
 
 template <typename T>
-size_t TimSort<T>::minRunLength(size_t len) {
-    size_t sticky = 0;
-    while(len >= (1 << 6)) {
-        sticky |= (len & 1);
-        len >>= 1;
-    }
+typename TimSort<T>::size_t TimSort<T>::minRunLength(size_t len) {
+	size_t sticky = 0;
+	while(len >= (1 << 6)) {
+		sticky |= (len & 1);
+		len >>= 1;
+	}
 
-    return len + sticky;
+	return len + sticky;
 }
 
 template <typename T>
-size_t TimSort<T>::upperbound(const T arr[], const T &val, size_t l, size_t r, Comparer cmp) {
-    size_t mid;
-    while(l < r) {
-        mid = (l + r) / 2;
-        if(cmp(val, arr[mid])) {
-            r = mid;
-        }
-        else [
-            l = mid + 1;
-        ]
-    }
-    return cmp(arr[l], val) ? l : (l + 1);
+typename TimSort<T>::size_t TimSort<T>::upperbound(const T arr[], const T &val, size_t l, size_t r, Comparer cmp) {
+	size_t mid;
+	while(l < r) {
+		mid = (l + r) / 2;
+		if(cmp(val, arr[mid])) {
+			r = mid;
+		}
+		else {
+			l = mid + 1;
+		}
+	}
+	return cmp(val, arr[l]) ? l : (l + 1);
 }
 
 template <typename T>
 void TimSort<T>::insertSort(T arr[], size_t len, Comparer cmp) {
-    size_t pos;
-    for(size_t i = 1; i < len; i++) {
-        if(!cmp(arr[i], arr[i - 1])) {
-            continue;
-        }
-        pos = upperbound(arr, arr[i], 0, i - 1, cmp), temp = arr[i];
-        for(size_t j = i; j > pos; j--) {
-            arr[j] = arr[j - 1];
-        }
-        arr[pos] = temp;
-    }
+	size_t pos;
+	for(size_t i = 1; i < len; i++) {
+		if(!cmp(arr[i], arr[i - 1])) {
+			continue;
+		}
+		pos = upperbound(arr, arr[i], 0, i - 1, cmp), temp = arr[i];
+		for(size_t j = i; j > pos; j--) {
+			arr[j] = arr[j - 1];
+		}
+		arr[pos] = temp;
+	}
 }
--- bench.cpp
+++ bench.cpp
@@ -1,25 +1,27 @@
-#include "./MergeSort/mergesort.cpp"
+#include "./TimSort/timsort.cpp"
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 bool cmp(const int &a, const int &b) {
-    return a < b;
+	return a > b;
 }
 
 int main() {
-    MergeSort <int> mergesort;
-    int *arr = new int[100], len;
-    cin >> len;
-    for(int i = 0; i < len; i++) {
-        cin >> arr[i];
-    }
+	TimSort <int> sort;
+	int *arr = new int[100], len;
+	cin >> len;
+	for(int i = 0; i < len; i++) {
+		cin >> arr[i];
+	}
+	
+	vector <int> vec;
 
-    mergesort.sort(arr, len);
+	sort.sort(arr, len, cmp);
 
-    for(int i = 0; i < len; i++) {
-        cout << arr[i] << ' ';
-    }
+	for(int i = 0; i < len; i++) {
+		cout << arr[i] << ' ';
+	}
 
-    delete[] arr;
+	delete[] arr;
 }
