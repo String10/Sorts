a9781a0513ffd037d45a07908f306fdf9af2ac9d
String Max <mr.tens@foxmail.com>
2022-01-09 20:15:14 +0800

    Test stability, good.

--- bench.cpp
+++ bench.cpp
@@ -3,25 +3,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool cmp(const int &a, const int &b) {
-	return a < b;
+using test_type = pair <int, int>;
+
+bool cmp(const test_type &a, const test_type &b) {
+	return a.first < b.first;
 }
 
 int main() {
-	TimSort <int> sort;
-	int *arr = new int[100], len;
+	TimSort <test_type> sort;
+	test_type *arr = new test_type[100];
+	int len;
 	cin >> len;
 	for(int i = 0; i < len; i++) {
-		cin >> arr[i];
+		cin >> arr[i].first >> arr[i].second;
 	}
 	
-	vector <int> vec;
-
 	sort.sort(arr, len, cmp);
 
 	for(int i = 0; i < len; i++) {
-		cout << arr[i] << ' ';
+		cout << '#' << i << ':' << arr[i].first << ' ' << arr[i].second << endl;
 	}
 
 	delete[] arr;
 }
+
+/*
+15
+
+72 1
+45 1
+23 1
+53 1
+54 1
+63 1
+45 2
+44 1
+23 2
+45 3
+38 1
+45 4
+63 2
+36 1
+38 2
+*/
