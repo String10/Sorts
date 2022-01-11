08267b8404c491dc20bf1fa8d6317a4618ed930a
String Max <mr.tens@foxmail.com>
2022-01-11 11:46:47 +0800

    Edit bench.cpp to debug.

--- bench.cpp
+++ bench.cpp
@@ -3,25 +3,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using test_type = pair <int, int>;
+// using test_type = pair <int, int>;
+using test_type = int;
 
 bool cmp(const test_type &a, const test_type &b) {
-	return a.first < b.first;
+	// return a.first < b.first;
+	return a < b;
 }
 
 int main() {
+	// TimSort <test_type> sort;
+	// test_type *arr = new test_type[100];
+	// int len;
+	// cin >> len;
+	// for(int i = 0; i < len; i++) {
+	// 	cin >> arr[i].first >> arr[i].second;
+	// }
+	
+	// sort.sort(arr, len, cmp);
+
+	// for(int i = 0; i < len; i++) {
+	// 	cout << '#' << i << ':' << arr[i].first << ' ' << arr[i].second << endl;
+	// }
+
+	// delete[] arr;
+
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+
 	TimSort <test_type> sort;
-	test_type *arr = new test_type[100];
 	int len;
 	cin >> len;
+	test_type *arr = new test_type[len];
 	for(int i = 0; i < len; i++) {
-		cin >> arr[i].first >> arr[i].second;
+		cin >> arr[i];
 	}
 	
 	sort.sort(arr, len, cmp);
 
 	for(int i = 0; i < len; i++) {
-		cout << '#' << i << ':' << arr[i].first << ' ' << arr[i].second << endl;
+		cout << arr[i] << ' ';
 	}
 
 	delete[] arr;
